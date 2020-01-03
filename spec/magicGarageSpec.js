31969a5759078c8f19377170d485d5126cac4c52
Nicolas LE CHENIC <nicolaslechenic@gmail.com>
2020-01-03 10:48:13 +0100

    exo concatenate

---
+++ spec/magicGarageSpec.js
@@ -0,0 +1,29 @@
+import { magicGarage } from "../src/magicGarage";
+
+describe('magicGarage', () => {
+    it('return expected song for left ear ', () => {
+        let car = {
+            type: 'voiture',
+            color: 'bleu',
+            prefix: 'une'
+        };
+
+        let moto = {
+            type: 'moto',
+            color: 'rouge',
+            prefix: 'la'
+        };
+
+        let truck = {
+            type: 'camion',
+            color: 'gris',
+            prefix: 'un'
+        };
+
+
+        expect(magicGarage(car)).toEqual('Bravo, tu viens de réparer une voiture bleu !');
+        expect(magicGarage(moto)).toEqual('Bravo, tu viens de réparer la moto rouge !');
+        expect(magicGarage(truck)).toEqual('Bravo, tu viens de réparer un camion gris !');
+    });
+
+});
---
+++ src/magicGarage.js
@@ -0,0 +1,15 @@
+/*
+ * Le jouet "garage magique" détecte le véhicule dans le garage et vous dit ce qu'il répare !
+ *
+ * si :
+ * let vehicle = { type: "moto", color: "verte", prefix: "une" };
+ *
+ * alors le garage magic retournera :
+ * Bravo, tu viens de réparer une moto verte !
+ *
+ */
+function magicGarage(vehicle) {
+    // Code here !
+}
+
+export { magicGarage };
